14fc7103471a99766a29183df0053d285f8d43d9
Ariful Islam <hello.mrariful@gmail.com>
2024-05-22 00:14:06 +0600

    add mongo url in env file

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,11 @@
+const mongoose = require('mongoose');
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
+
+  async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  
+  }
